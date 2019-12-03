eb1dbef1073d27d9e8ff7a2e6fb6074b4c0ed370
Aylox <AyloxPro@hotmail.com>
2019-12-03 16:11:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,8 +20,21 @@ void explore(vector<std::string> & pages, vector<vector<bool>> & adjs, const uns
     if(pages.size() == current) return;
     std::string path = pages.at(current);
     std::string page = openFile(path);
-
-
+    std::regex reg("<a href=\"[^\"]+\"");
+    smatch matches;
+    string::const_iterator searchStart( page.cbegin() );
+    while ( regex_search( searchStart, page.cend(), matches, reg ) ) {
+      std::string link = matches[0];
+      link = link.substr(9, link.length()-10);
+      size_t pos = (std::find(pages.begin(), pages.end(), link) - pages.begin());
+      if(pos == pages.size()){
+          //Check if relative
+          pages.push_back(link);
+      }
+      adjs[current][pos] = true;
+      pages.push_back(link);
+      searchStart = matches.suffix().first;
+   }
     explore(pages, adjs, current+1);
 }
 
@@ -29,6 +42,7 @@ int main()
 {
     std::string path = "https://www.lmfdb.org/EllipticCurve/Q/";
     vector<std::string> pages = vector<std::string>();
+    vector<vector<bool>> matrices = vector<vector<bool>>();
     vector<vector<bool>> adjs = vector<vector<bool>>();
     pages.push_back("nawak");
     pages.push_back(path);
