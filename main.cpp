f63189afae556279f3460f2b0d04a0983fe5f7e7
Aylox <AyloxPro@hotmail.com>
2019-12-05 19:25:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 
 using namespace std;
 
+std::string urlBase = "www.lmfdb.org";
+
 std::string openFile(const std::string & path){
-    std::string fPath = "curl " + path + " > currentPage.txt";
+    std::string fPath = "curl -L " + path + " > currentPage.txt";
     system(fPath.c_str());
     ifstream ifs("currentPage.txt");
     std::string result;
@@ -16,51 +18,66 @@ std::string openFile(const std::string & path){
     return result;
 }
 
+bool startsWith(std::string mainStr, std::string toMatch) {
+    if(mainStr.find(toMatch) == 0)
+        return true;
+    else
+        return false;
+}
+
 void explore(vector<std::string> & pages, vector<vector<bool>> & adjs, const unsigned & current){
     if(pages.size() == current) return;
     std::string path = pages.at(current);
+    std::cout << path << std::endl;
+
+
     std::string page = openFile(path);
+
+    //Commencer a partir de body
+
     std::regex reg("<a href=\"[^\"]+\"");
     smatch matches;
     string::const_iterator searchStart( page.cbegin() );
     while ( regex_search( searchStart, page.cend(), matches, reg ) ) {
-      std::string link = matches[0];
-      link = link.substr(9, link.length()-10);
-      size_t pos = (std::find(pages.begin(), pages.end(), link) - pages.begin());
-      if(pos == pages.size()){
-          //Check if relative
-          pages.push_back(link);
-      }
-      adjs[current][pos] = true;
-      pages.push_back(link);
-      searchStart = matches.suffix().first;
-   }
+        std::string link = matches[0];
+        link = link.substr(9, link.length()-10);
+        std::cout << link << std::endl;
+        if(startsWith(link, "https://"))
+            link = link.substr(8);
+        if(startsWith(link, "http://"))
+            link = link.substr(7);
+        bool absolut = link.at(0) == '/';
+
+        size_t pos ;
+        if(!absolut && !startsWith(link, urlBase)){
+            pos = 0;
+            std::cout << "Site exterieur" << std::endl;
+        }else{
+            pos = (std::find(pages.begin(), pages.end(), link) - pages.begin());
+            if(pos == pages.size()){
+                link = (absolut ? urlBase : "") + link;
+                if(pages.size() != 10)
+                    pages.push_back(link);
+            }
+        }
+        adjs[current][pos] = true;
+        searchStart = matches.suffix().first;
+    }
     explore(pages, adjs, current+1);
 }
 
 int main()
 {
-    std::string path = "https://www.lmfdb.org/EllipticCurve/Q/";
+    std::string path = urlBase + "/EllipticCurve/Q";
     vector<std::string> pages = vector<std::string>();
-    vector<vector<bool>> matrices = vector<vector<bool>>();
-    vector<vector<bool>> adjs = vector<vector<bool>>();
+    vector<vector<bool>> matrices (1000, vector<bool> (1000));
     pages.push_back("nawak");
     pages.push_back(path);
-    explore(pages, adjs, 1);
+    explore(pages, matrices, 1);
 
-    string var = "first second third forth";
-
-    const regex r("(.*)");
-    smatch sm;
-
-    if (regex_search(var, sm, r))
-    {
-        for (int i=1; i<sm.size(); i++)
-        {
-            cout << sm[i] << endl;
-        }
+    for(const std::string & page : pages){
+        std::cout << page << std::endl;
     }
-
 
     std::cout << "End" << std::endl;
     return 0;
